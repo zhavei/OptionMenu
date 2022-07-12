ecaee5f3ee28c4b6bdf5724a3d350e55319ac99d
zhavei <zhavei.vei@gmail.com>
2022-07-12 22:08:32 +0700

    add

--- app/src/main/java/com/syafei/optionmenu/activity/MainActivity.kt
+++ app/src/main/java/com/syafei/optionmenu/activity/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.syafei.optionmenu.activity
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.syafei.optionmenu.R
 import com.syafei.optionmenu.fragment.MenuFragment
 
@@ -19,6 +23,27 @@ class MainActivity : AppCompatActivity() {
 
         val inflater = menuInflater
         inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search_appbar).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            /** Gunakan method ini ketika search selesai atau OK **/
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                searchView.clearFocus()
+                return true
+            }
+
+            /**Gunakan method ini untuk merespon tiap perubahan huruf pada searchView */
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+
         return true
     }
 
