c31ae60393d51820145819d6aa1d4dc31669bc49
zhavei <zhavei.vei@gmail.com>
2022-07-18 15:53:08 +0700

    testing camera

--- app/src/main/java/com/syafei/optionmenu/activity/ThirdActivity.kt
+++ app/src/main/java/com/syafei/optionmenu/activity/ThirdActivity.kt
@@ -1,13 +1,20 @@
 package com.syafei.optionmenu.activity
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.syafei.optionmenu.BuildConfig
-import com.syafei.optionmenu.R
 import com.syafei.optionmenu.data.model.ApiService
 import com.syafei.optionmenu.data.model.DataItem
 import com.syafei.optionmenu.data.model.ResponseUser
+import com.syafei.optionmenu.databinding.ActivityThirdBinding
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
@@ -18,17 +25,27 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ThirdActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityThirdBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_third)
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         getDataRetrofit()
         getLoggingInteceptor()
         getLoggingChucker()
 
-
+        binding.pickImageButton.setOnClickListener {
+            pickImageFromGallery()
+        }
+        binding.btCapturePhoto.setOnClickListener {
+            openCamera()
+        }
     }
 
+    //region checker example
     private fun getLoggingChucker() {
         val clientChucker = OkHttpClient.Builder().addInterceptor(ChuckerInterceptor(this)).build()
         val retrofitChuk =
@@ -70,9 +87,73 @@ class ThirdActivity : AppCompatActivity() {
             }
         })
     }
+    //endregion
+
+    // region 2nd pick camera
+    private fun openCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+            intent.resolveActivity(packageManager)?.also {
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_PERMISSION
+            )
+        }
+    }
+    //endregion
+
+    // region 1st image pick galery
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+    // endregion
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // region 1st pick galery
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+            binding.imageView.setImageURI(data?.data)
+        }
+        // endregion
+        //region 2nd pick from camera
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                val bitmap = data?.extras?.get("data") as Bitmap
+                binding.imageView.setImageBitmap(bitmap)
+            } else if (requestCode == REQUEST_PICK_IMAGE) {
+                val uri = data?.getData()
+                binding.imageView.setImageURI(uri)
+            }
+        }
+        //endregion
+    }
 
     companion object {
         const val BASE_URL = "https://reqres.in/"
         val TAG: String = ThirdActivity::class.java.simpleName
+
+        val IMAGE_REQUEST_CODE = 1_000;
+
+        private val REQUEST_PERMISSION = 100
+        private val REQUEST_IMAGE_CAPTURE = 1
+        private val REQUEST_PICK_IMAGE = 2
     }
+
+    override fun onResume() {
+        super.onResume()
+        checkCameraPermission()
+    }
+
+
 }
